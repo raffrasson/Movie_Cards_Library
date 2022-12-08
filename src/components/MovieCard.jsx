9b28e79573203c9ee4b404a64e3970db9738afc2
raffrasson <raf.frasson@gmail.com>
2022-12-08 19:58:08 -0300

    uploads project to Github

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,16 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <div>
-        <h4>{ movie.title }</h4>
-        <h5>{ movie.subtitle }</h5>
-        <img src={ movie.imagePath } alt="imagemdofilme" />
-        <p>{ movie.storyline }</p>
-        <Rating rating={ movie.rating } />
+      <div className="movie-card">
+        <div className="movie-card-header">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+        </div>
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={ movie.imagePath } alt="imagem do filme" />
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+          <Rating className="movie-card-rating" rating={ movie.rating } />
+        </div>
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import propTypes from 'prop-types';
 import MovieCard from './MovieCard';
+import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
-    return movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
+    return (
+      <div className="movie-list">
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      </div>
+    );
   }
 }
-
-MovieList.propTypes = {
-  movies: propTypes.arrayOf(propTypes.object).isRequired,
-};
 
 export default MovieList;
